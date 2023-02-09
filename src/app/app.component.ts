b309ef355a57039e5471d57c8c656cbd218d2ece
alvaroverdu <alvaroverdpina@gmail.com>
2023-02-09 19:16:13 +0100

    primer commit para subir a versel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Tweet } from './share/model/tweet';
+import { TweetComponent } from './share/tweet/tweet.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'twitter-web3';
+
+  public tweets : Tweet[] = [];
+
+  constructor() {
+    let tweet = new Tweet(new Date(),"Tweet1","Miguel",25,[],[],[]);
+    this.tweets.push(tweet);
+    tweet = new Tweet(new Date(),"Tweet2","Juan",25,[],[],[]);
+    this.tweets.push(tweet);
+    tweet = new Tweet(new Date(),"Tweet3","Pedro",25,[],[],[]);
+    this.tweets.push(tweet);
+    tweet = new Tweet(new Date(),"Tweet4","Ramon",25,[],[],[]);
+    this.tweets.push(tweet);
+  }
+
 }
